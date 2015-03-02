c10f017948bb9120bd305de4381895d63ae24b02
paulwangtao <wangtaovipone@sina.com>
2015-03-02 22:04:04 +0800

    First Commit
    First commit

---
+++ expectApp/expectApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  expectApp
+//
+//  Created by wangtao on 15/3/2.
+//  Copyright (c) 2015年 王涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
